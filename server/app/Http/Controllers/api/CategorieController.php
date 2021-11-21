9f0c3e2ff465ece3a9518f347747a76a6366397e
Alexis Fernando Rodriguez Quezada <2172394@my.ipleiria.pt>
2021-11-21 21:35:27 +0000

    CategorieResource implemented

--- server/app/Http/Controllers/api/CategorieController.php
+++ server/app/Http/Controllers/api/CategorieController.php
@@ -5,12 +5,13 @@ namespace App\Http\Controllers\api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\VCard;
+use App\Http\Resources\CategorieResource;
 
 class CategorieController extends Controller
 {
     //
     public function getCategoriesOfVcard(Vcard $vcard) 
     { 
-        return $vcard->categories; 
+        return CategorieResource::collection($vcard->categories); 
     }
 }
