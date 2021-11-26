d7ccbdeb2cfb8727adf6819ed41e1f6d4184ff72
Gerson Aquino <gerson_aquino10@hotmail.com>
2021-11-26 16:01:55 +0000

    Squashed commit of the following:
    commit 7781de16fb51d4e3519900c3f962c5d00b56ccad
    Author: Gerson Aquino <gerson_aquino10@hotmail.com>
    Date:   Fri Nov 26 15:58:16 2021 +0000
    
        Revert "Revert "Fix the error.""
    
        This reverts commit 2a5cb16c3f05ba94a2d59b380d733abd45486891.
    
    commit 2a5cb16c3f05ba94a2d59b380d733abd45486891
    Author: Gerson Aquino <gerson_aquino10@hotmail.com>
    Date:   Fri Nov 26 15:56:57 2021 +0000
    
        Revert "Fix the error."
    
        This reverts commit 868eaadda915dc6bb4913c73133b3b9842c382a5.
    
    commit 343186bef968969c58402f8a95f6a529c9c3bf25
    Author: Gerson Aquino <gerson_aquino10@hotmail.com>
    Date:   Fri Nov 26 15:56:50 2021 +0000
    
        Revert "Fix the error."
    
        This reverts commit c45551ddb4edb4fb81345b90c3560267836f332b, reversing
        changes made to 868eaadda915dc6bb4913c73133b3b9842c382a5.
    
    commit c45551ddb4edb4fb81345b90c3560267836f332b
    Merge: 868eaad c68935e
    Author: gfrancv <gfrancv@hotmail.com>
    Date:   Fri Nov 26 15:36:43 2021 +0000
    
        Fix the error.
    
    commit 868eaadda915dc6bb4913c73133b3b9842c382a5
    Author: gfrancv <gfrancv@hotmail.com>
    Date:   Fri Nov 26 15:35:23 2021 +0000
    
        Fix the error.

--- client/src/components/transactions/Transaction.vue
+++ client/src/components/transactions/Transaction.vue
@@ -7,80 +7,79 @@
 </template>
 
 <script>
-import TransactionDetail from "./TransactionDetail.vue";
+import TransactionDetail from "./TransactionDetail.vue"
 
 export default {
-  name: "Transactions",
-  components: {
-    TransactionDetail,
-  },
-
-  props: {
-    vcard: {
-      type: String,
-      default: null,
+    name: 'Transactions',
+    components: {
+      TransactionDetail
     },
-    idTransaction: {
-      type: Number,
-      default: null,
-    },
-  },
 
-  data() {
-    return {
-      transaction: this.newTransaction(),
-      errors: null,
-    };
-  },
-
-  computed: {
-    operation() {
-      return !this.idTransaction || this.idTransaction < 0
-        ? "insert"
-        : "update";
-    },
-  },
-
-  watch: {
-    // beforeRouteUpdate was not fired correctly
-    // Used this watcher instead to update the ID
-    idTransaction: {
-      immediate: true,
-      handler(newValue) {
-        this.loadTask(newValue);
+    props: {
+      vcard: {
+        type: Number,
+        default: null,
+      },
+      idTransaction: {
+        type: Number,
+        default: null,
       },
     },
-  },
 
-  methods: {
-    newTransaction() {
-      return {
-        idTransaction: null,
-        owner_id: this.vcard,
-        description: "",
-        notes: "",
-      };
+    data() {
+        return {
+          transaction: this.newTransaction(),
+          errors: null,
+        }
     },
 
-    loadTask(idTransaction) {
-      this.errors = null;
-      if (!idTransaction || idTransaction < 0) {
-        this.transaction = this.newTransaction();
-        //this.originalValueStr = this.dataAsString()
-      } else {
-        this.$axios
-          .get("vcard/" + this.vcard + "/transactions/" + this.idTransaction)
-          .then((response) => {
-            this.task = response.data.data;
-            this.originalValueStr = this.dataAsString();
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-      }
+    computed: {
+      operation () {
+        return !this.idTransaction || this.idTransaction < 0 ? "insert" : "update"
+      },
     },
-  },
-};
+
+    watch: {
+      // beforeRouteUpdate was not fired correctly
+      // Used this watcher instead to update the ID
+      idTransaction: {
+        immediate: true,
+        handler (newValue) {
+          this.loadTask(newValue)
+        },
+      },
+    },
+
+    methods: {
+      newTransaction () {
+        return {
+          idTransaction: null,
+          owner_id: this.$vcard,
+          description: "",
+          notes: "",
+        }
+      },
+
+      loadTask (idTransaction) {
+        this.errors = null
+        if (!idTransaction || idTransaction < 0) {
+          this.transaction = this.newTransaction()
+          //this.originalValueStr = this.dataAsString()
+        } 
+        else {
+          this.$axios
+            .get("transactions/" + this.$vcard )
+            .then((response) => {
+              this.task = response.data.data
+              this.originalValueStr = this.dataAsString()
+            })
+            .catch((error) => {
+              console.log(error)
+            })
+        }
+      },
+    }
+}
 </script>
 
 <style scoped lang="scss">
