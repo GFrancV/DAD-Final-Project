9eeb6a32fac0941b4de7e3ce414350da86e742c1
Alexis Fernando Rodriguez Quezada <2172394@my.ipleiria.pt>
2021-11-27 20:55:22 +0000

    Fix Transaction show

--- server/app/Http/Controllers/api/TransactionController.php
+++ server/app/Http/Controllers/api/TransactionController.php
@@ -21,8 +21,8 @@ class TransactionController extends Controller
 
     public function show(Vcard $vcard, Transaction $transaction)
     {
-        $transaction = Transaction::where('vcard', $vcard->phone_number)->first();
-        return new TransactionResource($transaction);
+        $transactionSend = Transaction::where('id',$transaction->id)->first();
+        return new TransactionResource($transactionSend);
     }
 
     public function store(StoreUpdateTransactionRequest $request, Vcard $vcard)
